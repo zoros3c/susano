4050249a71decb93244d6e8442a750b6d729911e
zoros3c <167875160+zoros3c@users.noreply.github.com>
2024-04-23 19:16:32 +0530

    Add files via upload

---
+++ requirements.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# WHOIS installation
+echo "Installing WHOIS tool or Updating if already exists"
+apt install whois
+
+# naabu installation
+echo "Installing naabu or Updating if already exists"
+apt install naabu
+
+# Gospider installation
+echo "Installing gosider or  Updating if already exists"
+apt install gospider
+
+# pip installation
+echo "installing pip or Updaating if already exists"
+apt install pip
+
+# uro python package installation
+echo "Installing python package uro"
+pip install uro
+
+# Subfinder installation
+echo "Installing Subfinder or Updating if already exists"
+apt install subfinder
+
+# Eyeitness Installation
+apt install eyewitness 
+
+# Adding executable permissions
+#chmod +x susano
+#mv susano /bin
+
