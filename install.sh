459e5d9cde41efb0ca1c049298abad48b6e28e58
zoros3c <167875160+zoros3c@users.noreply.github.com>
2024-06-03 13:03:36 +0530

    install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
+# Download the Go tarball
+wget https://go.dev/dl/go1.20.4.linux-amd64.tar.gz
+
+# Extract it to /usr/local
+sudo tar -C /usr/local -xzf go1.20.4.linux-amd64.tar.gz
+
+# Add Go to the PATH
+echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.profile
+
+# Reload the profile
+source ~/.profile
+
+# Verify the installation
+go version
+
 # WHOIS installation
 echo "Installing WHOIS tool or Updating if already exists"
 apt install whois
 
-
 pip install fade
+
+# Install Katana 
+go install github.com/projectdiscovery/katana/cmd/katana@latest
 
 # Httpx-toolkit Installation 
 echo "Installing Httpx-toolkit or Updating if already exists"
